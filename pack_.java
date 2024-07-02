eee51ff0c0473695a030b22601cd8018041325c7
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-02 15:21:20 +0500

    pack_.java

---
+++ pack_.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class Beta {
+    public static void displayMessage() {
+        System.out.println("displayMessage of Beta");
+    }
+
+    public static void main(String[] args) {
+        Beta instance = new Beta();
+        instance.displayMessage();
+    }
+}
