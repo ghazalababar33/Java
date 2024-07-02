0b5c081d4ce2f22f299613e91792faebbd261ee7
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-03 01:05:52 +0500

    Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,67 @@
-public class Main {
+public class Person {
 
-    // Instance variables
     private String name;
-    private int age;
+    private SimpleDate birthday;
+    private int weight = 0;
+    private int height = 0;
 
-    // Constructor
-    public Main(String name, int age) {
+    public Person(String name, SimpleDate date) {
         this.name = name;
-        this.age = age;
+        this.birthday = date;
     }
 
-    // Instance method to display information
-    public void displayInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
+    public Person(String name, int day, int month, int year) {
+        this.name = name;
+        this.birthday = new SimpleDate(day, month, year);
     }
 
-    // Main method
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public SimpleDate getBirthday() {
+        return this.birthday;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", born on " + this.birthday;
+    }
+
+    public boolean olderThan(Person compared) {
+        return this.birthday.before(compared.getBirthday());
+    }
+
     public static void main(String[] args) {
-        // Create object 1
-        Main obj1 = new Main("Alice", 25);
-        obj1.displayInfo();
+        SimpleDate date = new SimpleDate(1, 1, 780);
+        Person muhammad = new Person("Muhammad ibn Musa al-Khwarizmi", date);
+        Person pascal = new Person("Blaise Pascal", 19, 6, 1623);
 
-        // Create object 2
-        Main obj2 = new Main("Bob", 30);
-        obj2.displayInfo();
+        System.out.println(muhammad);
+        System.out.println(pascal);
+
+        if (muhammad.olderThan(pascal)) {
+            System.out.println(muhammad.getName() + " is older than " + pascal.getName());
+        } else {
+            System.out.println(muhammad.getName() + " is not older than " + pascal.getName());
+        }
+
+        SimpleDate d1 = new SimpleDate(14, 2, 2011);
+        SimpleDate d2 = new SimpleDate(21, 2, 2011);
+        SimpleDate d3 = new SimpleDate(1, 3, 2011);
+        SimpleDate d4 = new SimpleDate(31, 12, 2010);
+
+        System.out.println(d1 + " is earlier than " + d2 + ": " + d1.before(d2));
+        System.out.println(d2 + " is earlier than " + d1 + ": " + d2.before(d1));
+
+        System.out.println(d2 + " is earlier than " + d3 + ": " + d2.before(d3));
+        System.out.println(d3 + " is earlier than " + d2 + ": " + d3.before(d2));
+
+        System.out.println(d4 + " is earlier than " + d1 + ": " + d4.before(d1));
+        System.out.println(d1 + " is earlier than " + d4 + ": " + d1.before(d4));
     }
 }
