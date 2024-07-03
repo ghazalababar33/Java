257701fe3755bbb7cbdff899ef34f9de941eb763
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-03 17:22:04 +0500

    BookList.java

---
+++ BookList.java
@@ -0,0 +1,15 @@
+package com.example.Assignment;
+
+import java.util.List;
+public class BookList {
+	
+	    private List<Books> books;
+
+	    public List<Books> getBooks() {
+	        return books;
+	    }
+
+	    public void setBooks(List<Books> books) {
+	        this.books = books;
+	    }
+	}
