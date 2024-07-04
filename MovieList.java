6488b305e1a094fe2fc9c1899d6b6dfad684c420
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-04 23:59:19 +0500

    Created_MovieList.java

---
+++ MovieList.java
@@ -0,0 +1,16 @@
+package com.example.Assignment;
+
+import java.util.List;
+
+public class MovieList {
+
+    private List<Movie> movies;
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+}
