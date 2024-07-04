c6bf409e19dc35f7f07215922cbf6620c20f90cf
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-04 23:56:03 +0500

    Created_MagazineList.java

---
+++ MagazineList.java
@@ -0,0 +1,16 @@
+package com.example.Assignment;
+
+import java.util.List;
+
+public class MagazineList {
+
+    private List<Magazine> magazines;
+
+    public List<Magazine> getMagazines() {
+        return magazines;
+    }
+
+    public void setMagazines(List<Magazine> magazines) {
+        this.magazines = magazines;
+    }
+}
