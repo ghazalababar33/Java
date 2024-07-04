7c42bba67caff4774112739d0e007ea237f8998e
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-04 23:48:32 +0500

    Created_AudioBookList.java

---
+++ AudioBookList.java
@@ -0,0 +1,16 @@
+package com.example.Assignment;
+
+import java.util.List;
+
+public class AudioBookList {
+
+    private List<AudioBook> audiobooks;
+
+    public List<AudioBook> getAudioBooks() {
+        return audiobooks;
+    }
+
+    public void setAudioBooks(List<AudioBook> audiobooks) {
+        this.audiobooks = audiobooks;
+    }
+}
