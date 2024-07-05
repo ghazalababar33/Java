e72e4ff57ddad3f3e926cbba3d45a28607c7ebfc
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-05 10:59:18 +0500

    BookJsons.java

--- BookJson.java
+++ BookJson.java
@@ -1,16 +1,16 @@
 package com.example.Assignment;
+
 import java.util.*;
 import com.google.gson.Gson;
-
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-public class BookJson {
+public class BookJsons {
 
     public static List<Books> parseBooksJson(String filePath) {
         Gson gson = new Gson();
-        try (Reader reader = new InputStreamReader(BookJson.class.getResourceAsStream(filePath))) {
-            BookList bookList = gson.fromJson(reader, BookList.class);
+        try (Reader reader = new InputStreamReader(BookJsons.class.getResourceAsStream(filePath))) {
+            BookLists bookList = gson.fromJson(reader, BookLists.class);
             return bookList.getBooks();
         } catch (Exception e) {
             e.printStackTrace();
